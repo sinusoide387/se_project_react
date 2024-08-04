ba715883d64e2acfa3188f1cca494a899c984c69
Franco <franco_t92@hotmail.com>
2024-08-04 09:39:18 -0400

    working on main.css styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,30 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
+import "../Main/Main.css";
 function Main() {
   console.log(defaultClothingItems);
   return (
     <div>
       <WeatherCard />
-      <section className="cards"></section>
-      <p className="cards__text">Today is 75° F / You may want to wear:</p>
-      <ul className="class__list">
-        {defaultClothingItems.map((item) => {
-          return (
-            <div key={item._id}>
-              <h2>{item.name}</h2>
-              <img src={item.link} alt={item.name}></img>
-            </div>
-          );
-        })}
-      </ul>
+      <section className="cards">
+        <p className="cards__text">Today is 75° F / You may want to wear:</p>
+        <div className="cards__list-container">
+          <ul className="cards__list">
+            {defaultClothingItems.map((item) => {
+              return (
+                <li key={item._id}>
+                  <h2 className="card__name">{item.name}</h2>
+                  <img
+                    className="card__image"
+                    src={item.link}
+                    alt={item.name}
+                  ></img>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
